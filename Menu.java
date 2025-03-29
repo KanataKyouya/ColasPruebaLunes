4b8d93ca57a96c252208ea07795d61544e13dc28
Kanata Kyouya <franciscotaborda7710@gmail.com>
2025-03-29 09:47:19 -0500

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,4 +1,10 @@
-// Francisco Andres Taborda Amaya, grupo 580506004-19 
+/* Francisco Andres Taborda Amaya, grupo 580506004-19
+    Nota: Buenas profe, aqui le mando el punto de colas que nos dejó hoy, 
+    aunque la verdad tengo mis dudas ya que no utiliza casi las mecanicas 
+    de la estructura de colas, ya que el prestar y devolver se hacian facil 
+    con un update, pero en si el programa funciona bien, espero que si esté 
+    conforme a lo que usted busca evaluar 
+*/
 
 
 public class Menu {
